9a5b86e63a175f1c5709f5cca46cc023a5e1a6fc
Brian Leishman <b@rian.gg>
2025-09-19 15:20:04 -0400

    feat: add LICENSE file and initial documentation

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Command swoof moves MySQL tables between databases using transactional swaps
+// and optional glob-style patterns so long imports avoid downtime. Run
+// `swoof -h` to review available flags.
+package main
